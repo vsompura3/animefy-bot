265eda8cf4682783bd40667bb4f22bc1ae563cde
Vaibhav Sompura <vsompura3@gmail.com>
2023-06-26 00:14:15 +0530

    added telegra.ph functionality of creating pages for each individual response

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
-import Telegraph from 'telegra.ph'
+// import Telegraph from 'telegra.ph'
 import { Context, Telegraf } from 'telegraf'
 import { helpCommands, welcomeMessage } from './constants/constant.js'
 import { Handler } from './utils/handler.js'
@@ -7,7 +7,26 @@ import { Handler } from './utils/handler.js'
 dotenv.config()
 
 const bot: Telegraf<Context> = new Telegraf(process.env.BOT_TOKEN as string)
+// client
+//   .createAccount()
+//   .then((account: any) => {
+//     client.token = account.access_token
+//     return client.getPageList()
+//   })
+//   .then((pages: any) => console.log(pages))
 
+// fetch(
+//   `https://api.telegra.ph/createPage?access_token=d3b25feccb89e508a9114afb82aa421fe2a9712b963b387cc5ad71e58722&title=Sample+Page&author_name=Anonymous&content=[{"tag":"p","children":["Hello,+world!"]}]&return_content=true`,
+// )
+
+// client.createAccount().then((account: any) => {
+//   client.token = process.env.BOT_TOKEN
+//   return client.getPageList()
+// })
+// bot.use((ctx, next): Promise<void> => {
+//   console.log('Recieved a message', ctx?.message)
+//   return next()
+// })
 bot.command('start', ctx => {
   ctx.telegram.sendMessage(ctx.message.chat.id, welcomeMessage, {
     reply_to_message_id: ctx.message.message_id,
--- src/utils/handler.ts
+++ src/utils/handler.ts
@@ -1,9 +1,18 @@
+import Telegraph from 'telegra.ph'
 import { Context } from 'telegraf'
+import { author, botName } from '../constants/constant.js'
 import { SubpleaseAnimeResponse } from '../types/types.js'
 import { sleep } from './helper.js'
 import { recentAnimes } from './recentAnimes.js'
 
 async function Handler(ctx: Context, editMessageId: number) {
+  let telegraph_access_token: string = ''
+  await fetch(
+    `https://api.telegra.ph/createAccount?short_name=${botName}&author_name=${botName}}`,
+  )
+    .then(res => res.json())
+    .then(data => (telegraph_access_token = data.result.access_token))
+
   const res: SubpleaseAnimeResponse | null = await recentAnimes()
 
   if (res === null) {
@@ -19,15 +28,34 @@ async function Handler(ctx: Context, editMessageId: number) {
   for (const key in res) {
     if (res.hasOwnProperty(key)) {
       const dataObj = res[key]
-      console.log(dataObj)
+      console.log(telegraph_access_token)
       msg += `\n============ ${dataObj.time} Episode ============\n*${dataObj.show}*\nEpisode: [${dataObj.episode}]\nDownload Links: `
       for (let idx in dataObj.downloads) {
-        msg += `[${dataObj.downloads[idx].res}p](http://www.example.com/) \t\t`
+        let magnetURL: string = 'https://github.com/vsompura3/animefy-bot'
+        await fetch(
+          `https://api.telegra.ph/createPage?access_token=${telegraph_access_token}&title=${
+            dataObj.show +
+            ' ' +
+            dataObj.episode +
+            ' ' +
+            dataObj.downloads[idx].res
+          }
+        }&author_name=${
+          botName + ' - ' + author
+        }&content=[{"tag":"a","attrs":{ "href":"https://github.com"},"children":["Nakamura"]}]&return_content=true`,
+        )
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            magnetURL = data.result.url
+            msg += `[${dataObj.downloads[idx].res}p](${magnetURL}) \t\t`
+          })
+        await sleep(100)
       }
       msg += `\nRelease Date: ${new Date(
         dataObj.release_date,
       ).toLocaleDateString('en-GB')}\n`
-      await sleep(200)
+      await sleep(500)
       ctx.telegram.editMessageText(
         ctx!.chat?.id,
         editMessageId,
